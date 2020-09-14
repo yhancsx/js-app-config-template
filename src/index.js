31988f3349b09216a7b52638f03fc6e4822962dc
yhan-bae <yhan.bae@samsung.com>
2020-09-14 11:11:55 +0900

    hello world

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import '../index.scss';
+
+const app = document.createElement('div');
+app.innerText = 'hello world';
+app.className = 'hello-world';
+const root = document.getElementById('root');
+root.appendChild(app);
