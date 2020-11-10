cfdd5ebdb06e89fdef94160abfbcca317be42d16
johnbae93 <johnbae57@naver.com>
2020-11-10 23:26:58 +0900

    react-hot-loader

--- index.jsx
+++ index.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './src/App';
 
-const app = () =>
+const app = () => {
   ReactDOM.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
     document.getElementById('root'),
   );
+};
 
 app();
-
-if (module.hot) {
-  module.hot.accept('./src/App', app);
-}
